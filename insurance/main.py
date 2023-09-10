506f1ac6fcd2c3a9abcde692020b84ccc10fd9bc
Sergey Bogdanov <bogs@yandex.ru>
2023-09-10 15:13:31 +0300

    add mayn.py

---
+++ insurance/main.py
@@ -0,0 +1,6 @@
+
+amount = float(input("Сумма кредита? "))
+insurance = float(input("Сумма страховки? ")])
+monthly_payment = float(input("Ежемесячный платеж? "))
+annual_rate = float(input("Годовая ставка процента? "))
+credit_term = float(input("Срок кредита? "))
