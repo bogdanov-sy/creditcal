dce3252839fa2a367d2eb775c696b7dae7e3dba7
bogdanov.sy <bogs@yandex.ru>
2023-09-15 11:26:01 +0300

    refactoring main.py

--- insurance/main.py
+++ insurance/main.py
@@ -1,10 +1,13 @@
-from . import options as opt
+from options import calculatecredit
+
+def main():
+    amount_of_credit = float(input("Amount of credit: "))
+    insurance = float(input("Insurance: "))
+    monthly_payment = float(input("Monthly payment: "))
+    annual_rate = float(input("Annual rate: "))
+    credit_term = float(input("credit_term: "))
+    result = calculatecredit(amount_of_credit, insurance, monthly_payment, annual_rate, credit_term)
+    print(result)
 
 
-amount = float(input("Сумма кредита? "))
-insurance = float(input("Сумма страховки? ")])
-monthly_payment = float(input("Ежемесячный платеж? "))
-annual_rate = float(input("Годовая ставка процента? "))
-credit_term = float(input("Срок кредита? "))
-
-def calculatecredit(request):
+main()
--- insurance/options.py
+++ insurance/options.py
@@ -39,36 +39,25 @@ def selection_annual_rate(amount, credit_term, monthly_payment):
 
 def calculatecredit(amount, insurance, monthly_payment, annual_rate, credit_term):
     if amount != 0 and  monthly_payment != 0 and annual_rate != 0 and credit_term != 0:
-    
-    
-    if amount == 0:
-        if monthly_payment == 0:
-            break
-        else:
-            amount = (monthly_payment * credit_term) - insurance
-        print(amount)
 
-    if monthly_payment == 0:
-        monthly_payment = opt.computation_monthly_payment(float(amount + insurance), float(annual_rate), int(credit_term))
+        real_rate = selection_annual_rate(amount, credit_term, monthly_payment)
+        s_real = monthly_payment * credit_term
+        overpaiment = str(s_real - amount)
+        overpaymentPercent = str((float(overpaiment) / float(amount)) * 100)
 
-    if insurance == 0:
-        monthly_payment_real = opt.computation_monthly_payment(amount, annual_rate, credit_term)
-        i = float(annual_rate) / 100 / 12
-        insurance = (monthly_payment - monthly_payment_real) / (i + (i / (((1 + i) ** credit_term) - 1)))
+        real_rateStr = str(numrus(float("{0:.1f}".format(float(real_rate)))))
+        insStr = str(numrus(float("{0:.1f}".format(float(insurance)))))
+        monthly_paymentStr = str(numrus(float("{0:.1f}".format(float(monthly_payment)))))
+        overpaimentStr = str(numrus(float("{0:.1f}".format(float(overpaiment)))))
+        overpaymentPercentStr = str(numrus(float("{0:.1f}".format(float(overpaymentPercent)))))
 
-    real_rate = opt.selection_annual_rate(amount, credit_term, monthly_payment)
-    s_real = monthly_payment * credit_term
-    overpaiment = str(s_real - amount)
-    overpaymentPercent = str((float(overpaiment) / float(amount)) * 100)
+        context = {'annualRateIns': real_rateStr, 'monthlyPayment': monthly_paymentStr,
+                       'overpaymentStr': overpaimentStr,
+                       'overpaymentPercent': overpaymentPercentStr, 'insuranceStr': insStr}
 
-    real_rateStr = str(opt.numrus(float("{0:.1f}".format(float(real_rate)))))
-    insStr = str(opt.numrus(float("{0:.1f}".format(float(insurance)))))
-    monthly_paymentStr = str(opt.numrus(float("{0:.1f}".format(float(monthly_payment)))))
-    overpaimentStr = str(opt.numrus(float("{0:.1f}".format(float(overpaiment)))))
-    overpaymentPercentStr = str(opt.numrus(float("{0:.1f}".format(float(overpaymentPercent)))))
+    else:
+        context = {'Не хватает данных'}
 
-    print("Реальный процент: " + str(real_rateStr))
-    print("Страховка: " + str(insStr))
-    print("Общая сумма переплаты: " + str(overpaimentStr))
+    return context
 
-    return 
+
